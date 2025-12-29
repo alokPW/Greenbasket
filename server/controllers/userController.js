0d90c4e05a180966bdfd42477bf6682187ef35ec
Alok pal <141651459+alokPW@users.noreply.github.com>
2025-12-29 21:41:31 +0530

    upadte the usercontroller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -122,6 +122,7 @@ export const logout=async(req,res)=>{
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            path : '/'
         }
 
         res.clearCookie("token", cookiesOption);
